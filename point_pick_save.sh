9e7b97c1604ae9e64eb1d7582357a60c7d4ba9bc
Bryan M Harvey <bmharvey@live.com>
2025-06-25 07:13:58 -0500

    remove redundant script

--- point_pick_save.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dir=$1 
-
-#python py/find_ridges.py -d $dir --x_bins 2048 --y_bins 2048 --sigma 2 --threshold 3.e-3
-python py/find_ridges.py -d $dir --x_bins 1024 --y_bins 1024 --sigma 3  --threshold 3.e-4
-python py/pick_clusters.py -d $dir
